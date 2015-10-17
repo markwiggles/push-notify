bd96525f691320fdd0b89d6e17047a217b8481bc
Mark Wigglesworth <markwigg@live.com>
2015-10-17 10:55:30 +1000

    removed active_admin and devise - to reinstall

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, 
+         :recoverable, :rememberable, :trackable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, 
+         :recoverable, :rememberable, :trackable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, 
+         :recoverable, :rememberable, :trackable, :validatable
 	has_many :devices, dependent: :destroy
 
 	accepts_nested_attributes_for :devices, allow_destroy: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+
   require 'sidekiq/web'
   mount Sidekiq::Web, at: '/sidekiq'
 
