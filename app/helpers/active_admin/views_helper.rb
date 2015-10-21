eb0dacff3963fa105adeba6027895cefd20181b7
Mark Wigglesworth <markwigg@live.com>
2015-10-21 17:06:20 +1000

    removed web_console whitelist

--- app/helpers/active_admin/views_helper.rb
+++ app/helpers/active_admin/views_helper.rb
@@ -3,8 +3,10 @@ module ActiveAdmin::ViewsHelper # camelized file name
   links = ''.html_safe
   links += link_to '<i class="fa fa-search fa-lg image-link"></i>'.html_safe, resource_path(resource)
   links += link_to '<i class="fa fa-edit fa-lg image-link"></i>'.html_safe, edit_resource_path(resource)
+	if Role.find(current_user.role_id).access_level == 1
   links += link_to '<i class="fa fa-close fa-lg image-link"></i>'.html_safe, resource_path(resource),
                    method: :delete, data: { confirm: 'Are you sure?' }, class: 'pull-right'
+	end
   links
 	end
 end
