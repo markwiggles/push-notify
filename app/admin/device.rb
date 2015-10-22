5bcb83d1dc0c7c95fcace12784fc0e3453168e76
Mark Wigglesworth <markwigg@live.com>
2015-10-22 11:16:05 +1000

    added permit params

--- app/admin/device.rb
+++ app/admin/device.rb
@@ -1,5 +1,8 @@
 ActiveAdmin.register Device do
 
+	permit_params :token, :platform, :enabled, :user_id
+
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
