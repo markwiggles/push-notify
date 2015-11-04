f8f9e09881083468ad5453bfb06f43cb923a2d08
Mark Wigglesworth <markwigg@live.com>
2015-11-05 08:18:35 +1000

    added default value for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+ before_save :set_default_access
+ # Include default devise modules. Others available are:
+ # :confirmable, :lockable, :timeoutable and :omniauthable
+ devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :trackable, :validatable
 
-	has_many :devices, dependent: :destroy
-	accepts_nested_attributes_for :devices, allow_destroy: true
+ has_many :devices, dependent: :destroy
+ accepts_nested_attributes_for :devices, allow_destroy: true
 
-# TODO email unique
+ def set_default_access
+  default_level = 2
+  self.role_id = (Role.find_by access_level: default_level).id if self.role_id.nil?
+  end
 end
